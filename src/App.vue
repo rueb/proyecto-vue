82f5d8eb222b89bdebdb75829e80076da8bcdf18
ruben dario <rueb.dario@gmail.com>
2025-01-23 07:02:37 -0400

    Crea Componente, Lista de tareas Borra Componentes Auo generados Crea Rutas

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 
 <template>
-    <Contador></Contador>
+    <MainView></MainView>
+  <!-- <Contador></Contador> -->  
+   <!--  <ListaDeTareas></ListaDeTareas> -->   
 </template>
 
 <script setup>
-import Contador from './components/Contador.vue';
+import MainView from './views/MainView.vue';
+//import Contador from './components/Contador.vue';
+//import ListaDeTareas from './components/ListaDeTareas.vue';
 </script>
 
 <style scoped>
---
+++ src/views/MainView.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <Sidebar></Sidebar>
+        <div class="content">
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import Sidebar  from '@/components/Sidebar.vue';
+
+</script>
+
+<style>
+    .container{
+        display: flex;
+    }
+    .content{
+        flex:1;
+        padding: 20 px;;
+    }
+</style>
